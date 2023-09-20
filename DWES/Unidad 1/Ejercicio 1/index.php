7b1faeddc6f779d82da5fb264bfc9c3047681e17
AlxCorp <alejandro@pfagot.com>
2023-09-20 10:10:09 +0200

    subida

--- DWES/Unidad 1/Ejercicio 1/index.php
+++ DWES/Unidad 1/Ejercicio 1/index.php
@@ -4,6 +4,6 @@
  </head>
  <body>
  <?php echo '<p>Hola Mundo</p>'; ?>
- <a href=""><button>Repositorio en GitHub</button></a>
+ <a href="https://github.com/AlxCorp/DAW2/blob/main/DWES/Unidad%201/Ejercicio%201/index.php"><button>Repositorio en GitHub</button></a>
  </body>
 </html>
