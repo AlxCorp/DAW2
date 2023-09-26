7fd8ef9e926b34a52f550718fb9fc4726f863b6d
AlxCorp <alejandro@pfagot.com>
2023-09-26 23:29:02 +0200

    sync

---
+++ DWES/Unidad 2/Actividad Teoría 1/README.md
@@ -0,0 +1,66 @@
+### Alejandro Priego Izquierdo
+
+1. Script que muestre el mensaje Hola Mundo entrecomillado.
+
+   ```php
+   <?php
+       echo '"hola mundo"';
+   ?>
+   ```
+
+2. Ficha personal con los datos cargados en variables. El resultado debe mostrar una foto personal.
+
+3. Script que, a partir del radio almacenado en una variable y la definición de la constante PI, calcule el área del círculo y la longitud de la circunferencia. El debe mostrar valor de radio, longitud de la circunferencia, área del círculo y dibujará un círculo utilizando gráficos vectoriales.
+
+4. ¿Cuál es la salida del siguiente script?
+
+```php
+    <?php
+        $ciclo="DAW";
+        $modulo="DWES";
+        print "<p>";
+        printf("%s es un módulo de %d curso de %s", $modulo, 2, $ciclo);
+        print "</p>";
+    ?>
+```
+
+Prueba el script y modifícalo para las palabras DAW y DWES apararezcan en negrita.
+Investiga uso de print y printf para salida de texto.
+
+5. Script que escriba el resultado de la suma de dos números almacenados en dosvariables.
+
+6. Script que cargue las siguientes variables:
+   $x=10;
+$y=7;
+   y muestre
+   10 + 7 = 17
+   10 - 7 = 3
+   10 \* 7 = 70
+   10 / 7 = 1.4285714285714
+   10 % 7 = 3
+
+7. Escribir un script que declare una variable y muestre la siguiente información en
+   pantalla:
+   Valor actual 8.
+   Suma 2. Valor ahora 10.
+   Resta 4. Valor ahora 6.
+   Multipica por 5. Valor ahora 30.
+   Divide por 3. Valor ahora 10.
+   Incrementa el valor en 1. Valor ahora 11.
+   Decrementa el valor en 1. Valor ahora 11.
+
+8. A veces es necesario conocer exactamente el contenido de una variable. Piensa
+   como puedes hacer esto y escribe un script con la siguiente salida:
+   string(5) “Harry”
+   Harry
+   int(28)
+   NULL
+
+9. Escribir un script que utilizando variables permita obtener el siguiente resultado:
+   Valor es string.
+   Valor es double.
+   Valor es boolean.
+   Valor es integer.
+   Valor is NULL.
+
+   10.Pon ejemplo de uso de la sintaxis heredoc en el manejo de cadenas.
---
+++ DWES/Unidad 2/Ejercicio 1/index.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Script para mostrar un portfolio basado en una plantilla
+Author: Alejandro Priego
+Date: 25/09/2023
+*/
+
+//Variables
+$email = "alejandor@pfagot.com";
+$phone = "617555743";
+$nombre = "Alejandro";
+$apellidos = "Priego Izquierdo";
+$linkedin = "https://www.linkedin.com/in/apriego/";
+$twitter = "https://www.twitter.com/priegoooo/";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alejandro Priego - Portfolio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <ul class="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm" id="pillNav2" role="tablist" style="--bs-nav-link-color: var(--bs-white); --bs-nav-pills-link-active-color: var(--bs-primary); --bs-nav-pills-link-active-bg: var(--bs-white);">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active rounded-5" id="home-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="true">Home</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link rounded-5" id="profile-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Profile</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link rounded-5" id="contact-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Contact</button>
+            </li>
+        </ul>
+    </header>
+
+    <main style="text-align: center">
+        <h1><?php echo $nombre, " ", $apellidos?></h1>
+        <h3>Teléfono: <?php echo $phone ?></h3>
+        <h3>Email: <?php echo $email ?></h3>
+        <h3>Linkedin: <a href="<?php echo $linkedin ?>">LINKEDIN</a></h3>
+        <h3>Twitter: <a href="<?php echo $twitter ?>">TWITTER</a></h3>
+    </main>
+</body>
+</html>
